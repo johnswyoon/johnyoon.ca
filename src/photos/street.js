1044b1e4c6fba70ffcdf3e9b9b3394b3e07a70a3
johnswyoon <john.sw.yoon@gmail.com>
2021-01-10 20:18:23 -0500

    image compress (eco)

--- src/photos/street.js
+++ src/photos/street.js
@@ -2,25 +2,25 @@ export const street = [
   {
     name: "td-square-shadow2020",
     url:
-      "https://res.cloudinary.com/jyoon/image/upload/q_auto:good/v1609649126/street/DSC03397-min_p0grt7.jpg",
+      "https://res.cloudinary.com/jyoon/image/upload/q_auto:eco/v1609649126/street/DSC03397-min_p0grt7.jpg",
     type: "card-tall",
   },
   {
     name: "ajay-stair-shadow-2020",
     url:
-      "https://res.cloudinary.com/jyoon/image/upload/q_auto:good/v1609649126/street/DSC03228-min_zjjdb3.jpg",
+      "https://res.cloudinary.com/jyoon/image/upload/q_auto:eco/v1609649126/street/DSC03228-min_zjjdb3.jpg",
     type: "card-tall",
   },
   {
     name: "st-lawrence-butcher-2020",
     url:
-      "https://res.cloudinary.com/jyoon/image/upload/q_auto:good/v1609649126/street/DSC03112-min_hixtpc.jpg",
+      "https://res.cloudinary.com/jyoon/image/upload/q_auto:eco/v1609649126/street/DSC03112-min_hixtpc.jpg",
     type: "card-tall",
   },
   {
     name: "coin-wash-smoker-2020",
     url:
-      "https://res.cloudinary.com/jyoon/image/upload/q_auto:good/v1609649126/street/DSC02837-Edit-min_vyzcu4.jpg",
+      "https://res.cloudinary.com/jyoon/image/upload/q_auto:eco/v1609649126/street/DSC02837-Edit-min_vyzcu4.jpg",
     type: "card-tall",
   },
 ];
