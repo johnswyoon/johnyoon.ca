f55b8328c2bc25537747ec6703509840f7121903
johnswyoon <john.sw.yoon@gmail.com>
2021-02-13 20:30:44 -0500

    post, delete endpoints created

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ extended: false }));
 
 //MongoDB
 connectDB();
 
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.send("Blog Post API");
 });
 
 //Routes
