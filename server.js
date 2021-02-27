3cd7aede8d887d8364f356ae2e518f63f2cc9c68
johnswyoon <john.sw.yoon@gmail.com>
2021-02-27 04:20:49 -0500

    deploy settings (still working on it)

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const connectDB = require("./config/db");
 require("dotenv").config();
 
+const path = require("path");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(cors());
@@ -12,14 +14,20 @@ app.use(express.json({ extended: false }));
 //MongoDB
 connectDB();
 
-app.get("/", (req, res) => {
-  res.send("Blog Post API");
-});
-
 //Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/posts", require("./routes/posts"));
 app.use("/api/users", require("./routes/users"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
