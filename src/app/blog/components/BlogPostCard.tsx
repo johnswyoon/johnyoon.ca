d2fd6a31c0cf61aacfa689439c93aa6b426ac6ed
John <55363559+johnswyoon@users.noreply.github.com>
2024-01-11 08:51:20 -0500

    [Feature] cleaned up blog grid

--- src/app/blog/components/BlogPostCard.tsx
+++ src/app/blog/components/BlogPostCard.tsx
@@ -15,20 +15,26 @@ export default function BlogPostCard({
 }: Post) {
   return (
     <Link href={`/blog/${slug}`}>
-      <div className="flex max-w-sm flex-col">
+      <div className="flex flex-col">
         <Image
           src={thumbnail as string}
-          className="mb-3 rounded-2xl"
+          className="mb-3 rounded-2xl object-cover"
+          style={{
+            boxShadow: '0px 3px 15px rgba(0, 0, 0, 0.2)',
+            minHeight: '225px',
+          }}
           alt={`${title} thumbnail`}
-          width={600}
-          height={400}
+          width={300}
+          height={225}
         />
         <h2 className="mb-2 text-xl font-bold">{title}</h2>
         <div className="flex justify-between">
           {tags && (
             <div className="flex gap-2">
               {tags.map((tag) => (
-                <Badge key={tag.id}>{tag.name}</Badge>
+                <Badge key={tag.id} className="h-7">
+                  {tag.name}
+                </Badge>
               ))}
             </div>
           )}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -5,20 +5,20 @@ import { getPosts } from '@/lib/posts';
 export default async function Blog() {
   const posts = await getPosts();
 
-  if (!posts) {
-    return null;
-  }
-
   return (
     <PageLayout>
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-medium">Blogaz</h1>
-        <div className="mt-10 grid grid-cols-3 gap-7">
-          {posts.map((post) => {
-            if (!post) return;
-            return <BlogPostCard key={post.id} {...post} />;
-          })}
-        </div>
+        {posts && posts.length > 0 ? (
+          <div className="mt-10 grid grid-cols-1 gap-7 sm:grid-cols-2 lg:grid-cols-3">
+            {posts.map((post) => {
+              if (!post) return;
+              return <BlogPostCard key={post.id} {...post} />;
+            })}
+          </div>
+        ) : (
+          <p>No posts yet!</p>
+        )}
       </div>
     </PageLayout>
   );
